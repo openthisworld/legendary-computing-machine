407b1230ecd37d517eeb33b293b747126cf503ab
openthisworld <puchok80@gmail.com>
2023-01-08 20:47:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from openai import ChatGPT
+import openai
 
+openai.api_key = "${{ secrets.API_KEY }}"   #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
 
-model = ChatGPT()
+def get_response(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
 
+    message = completions.choices[0].text
+    return message.strip()
 
-response = model.get_response("Ты здесь?")
-print(response)
+while True:
+    prompt = input("You: ")
+    response = get_response(prompt)
+    print("Bot:", response)
